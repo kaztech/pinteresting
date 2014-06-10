f1aea9f0352640cbfb5939594ad9707c1a995cfa
kaztech <ameen@ktg.biz>
2014-06-10 09:47:14 -0400

    ran heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   #required for heroku
   #set this to actual host
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://ktg-pinteresting.herokuapp.com' }
 end
